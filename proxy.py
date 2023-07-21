220ba7598e88273d04edd8c2df10536ef8fcb08d
Walter Gonzalez <walter.gonzalez@live.com.ar>
2023-07-21 15:41:37 -0300

    streaaming habilitado

--- proxy.py
+++ proxy.py
@@ -1,13 +1,15 @@
-from flask import Flask, request, jsonify
-import websockets
+from flask import Flask, Response, request, jsonify
 import time
 import poe
 import sys
 import time
 import json
+import atexit
 
 config=[]
 config_path = "config.json"
+aborted = False
+
 with open(config_path, 'r') as config_file:
     config = json.load(config_file)
 
@@ -49,6 +51,10 @@ def completions():
     # Obtener el mensaje de la solicitud y partimos el prompt cada 4000 caracteres para que poe lo pueda procesar bien
     messages = jsonToText(request.get_json())
     response = ""
+    stream = request.get_json()['stream']
+
+    if stream:
+        return Response(event_stream(messages), content_type='text/event-stream')
 
     for i in range(len(messages)):
         message = messages[i]
@@ -72,9 +78,57 @@ def completions():
                 client.purge_conversation(config['settings']['bot'], count=1)
                 break
 
-        print(message)
+        #print(message)
 
     return jsonify(response)
+
+def handle_abort(status = True):
+    global aborted
+    aborted = status
+
+@app.teardown_request
+def teardown_request(exception):
+    if exception:
+        handle_abort()
+
+def event_stream(messages):
+
+    response = {
+        "choices": [{
+            "delta": {
+                "role":"assistant",
+            }
+        }]
+    }
+    yield '\n\ndata: ' + json.dumps(response)
+    time.sleep(0.1)
+    prev_chunk = ""
+
+    for i in range(len(messages)):
+        message = messages[i]
+
+        if i == len(messages) - 1:
+            for chunk in client.send_message(config['settings']['bot'], message):
+                if aborted: #si le dan al boton stop, borramos el ultimo mensaje para cancelar la generacion (WIP)
+                    print ("Mensaje cancelado")
+                    client.purge_conversation(config['settings']['bot'], count=1)
+                    handle_abort(False)
+                    break
+
+                chunk = chunk["text_new"] 
+                if (chunk == ":" and prev_chunk == "U") : #esta intentando crear mensajes por nosotros, por ahora lo dejo asi pero probablemnte sea buena idea cancelar el mensaje
+                    pass
+                if chunk != "A" and not (chunk == ":" and prev_chunk == "A") : #evitamos enviar el A: que representa el inicio de mensajes de asistente.
+                    response = {"choices": [{"delta": {"content": chunk}}] }
+                    yield '\n\ndata: ' + json.dumps(response)
+                prev_chunk = chunk
+
+            yield '\n\ndata: [DONE]'
+        else: 
+            #estos son los primeros mensajes, se borran apenas se generan respuesta
+            for chunk in client.send_message(config['settings']['bot'], message):
+                client.purge_conversation(config['settings']['bot'], count=1)
+                break
 
 @app.route('/models', methods=['GET'])
 def models():
@@ -95,6 +149,8 @@ def models():
     }
     
     return jsonify(response)
+
+
 
 if __name__ == '__main__':
   app.run(port=5000)
